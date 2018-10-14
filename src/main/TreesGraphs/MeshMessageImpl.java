edd3276695eff0e15968a1fede379156748ca388
Manasa Kunaparaju <trainer3@trainer3s-MacBook-Pro.local>
2018-10-14 08:47:26 -0600

    committing files to exchange

---
+++ src/main/TreesGraphs/MeshMessageImpl.java
@@ -0,0 +1,15 @@
+package main.TreesGraphs;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MeshMessageImpl {
+
+    public static void main(String[] args) {
+        Queue<String> trial  = new LinkedList<>();
+        boolean isEmpty = isEmpty(trial);
+
+    }
+}
