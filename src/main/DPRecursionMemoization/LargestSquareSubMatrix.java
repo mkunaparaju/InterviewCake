d67c05fa3fa9fb227fc96e4ec3e93ab80d55f574
Manasa Kunaparaju <mkunaparaju@gmail.com>
2018-11-07 07:20:51 -0700

    square submatrix completed

--- src/main/DPRecursionMemoization/LargestSquareSubMatrix.java
+++ src/main/DPRecursionMemoization/LargestSquareSubMatrix.java
@@ -1,10 +1,14 @@
 package main.DPRecursionMemoization;
 
+
 public class LargestSquareSubMatrix {
+    static int [][] cache;
+
     public static void main(String[] args) {
         boolean [][] arr = {{true,true,true,true,true},{true, true,true,true,false},{true, true,true,true,false} ,{true, true,true,true,false} ,{true, false, false, false, false}};
-
-        System.out.println(squareSubmatrix(arr));
+        cache = new int [arr.length][arr[0].length];
+//        System.out.println(squareSubmatrix(arr));
+        System.out.println(bottomUpSquareSubmatrix(arr));
     }
 
     private static int squareSubmatrix(boolean[][] arr) {
@@ -12,7 +16,8 @@ public class LargestSquareSubMatrix {
         for(int i = 0 ; i < arr.length; i++){
             for (int j = 0 ; j < arr[0].length; j++ ){
                 if(arr[i][j]){
-                    max = Math.max(max, squareSubmatrix(arr, i , j));
+//                    max = Math.max(max, squareSubmatrix(arr, i , j));
+                    max = Math.max(max, cacheSquareSubmatrix(arr, i , j));
                 }
             }
         }
@@ -25,5 +30,29 @@ public class LargestSquareSubMatrix {
         if(!arr[i][j]) return 0;
 
         return 1 + Math.min(Math.min(squareSubmatrix(arr, i+1, j), squareSubmatrix(arr, i , j +1)), squareSubmatrix(arr, i+1, j+1));
+    }
+
+    private static int cacheSquareSubmatrix(boolean[][] arr, int i, int j){
+
+        if(i == arr.length || j == arr[0].length)return 0;
+        if(!arr[i][j]) return 0;
+        if(cache[i][j] > 0) return cache[i][j];
+        return 1 + Math.min(Math.min(squareSubmatrix(arr, i+1, j), squareSubmatrix(arr, i , j +1)), squareSubmatrix(arr, i+1, j+1));
+
+    }
+
+    private static  int bottomUpSquareSubmatrix(boolean [][] arr){
+        int max = 0;
+        for(int i = 0; i < arr.length; i ++){
+            for(int j = 0; j < arr[0].length; j ++){
+                if(i == 0 || j == 0) cache[i][j] = arr[i][j] ? 1:0;
+                else if(arr[i][j]) {
+                    cache[i][j] = 1 + Math.min(Math.min((cache[i-1][j]), cache[i][j-1]), cache[i-1][j-1]);
+                }
+               if(cache[i][j] > max) max = cache[i][j];
+            }
+        }
+
+        return max;
     }
 }
