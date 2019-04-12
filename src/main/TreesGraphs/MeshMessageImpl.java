c8b4620d48fbb68600481e39ae107be35126dde9
Manasa Kunaparaju <mkunaparaju@gmail.com>
2019-04-11 18:08:55 -0600

    update

--- src/main/TreesGraphs/MeshMessageImpl.java
+++ src/main/TreesGraphs/MeshMessageImpl.java
@@ -1,18 +1,30 @@
 package main.TreesGraphs;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class MeshMessageImpl {
 
     public static void main(String[] args) {
-        Queue<String> trial  = new LinkedList<>();
-        boolean isEmpty = isEmpty(trial);
+        Map<String, String[]> network = new HashMap<String, String[]>() {{
+            put("Min",     new String[] { "William", "Jayden", "Omar" });
+            put("William", new String[] { "Min", "Noam","Adam" });
+            put("Jayden",  new String[] { "Min", "Amelia", "Ren", "Noam" });
+            put("Ren",     new String[] { "Jayden", "Omar" });
+            put("Amelia",  new String[] { "Jayden", "Miguel" });
+            put("Adam",    new String[] { "Amelia", "Miguel", "Sofia", "Lucas" });
+            put("Miguel",  new String[] { "Amelia", "Adam", "Liam", "Nathan" });
+            put("Noam",    new String[] { "Nathan", "Jayden", "William" });
+            put("Omar",    new String[] { "Ren", "Min", "Scott" });
+        }};
+        MeshMessage mm = new MeshMessage();;
+        List<String> path = mm.getRoute(network, "Jayden", "Adam");
+
+        for(String name : path) System.out.print(name + " ");
     }
 
-    private static boolean isEmpty(Queue<String> trial) {
-        return false;
+    private List<String> getRoute(Map<String, String[]> network, String start, String end) {
+        HashMap<String, List<String>> visitedPathMap = new HashMap<>();
+        
+
     }
 }
